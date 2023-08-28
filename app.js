06838098c51d10ffb403b1de811c54935e244b63
lynxdm <lynxdev32@gmail.com>
2023-08-28 15:25:05 +0000

    added searching class

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const clearBtn = document.querySelector(".clear-btn");
 // ADD FOCUS ON CLICK
 wikiForm.addEventListener("click", () => {
   wikiSearch.focus();
+  wikiForm.classList.add("searching");
 });
 
 // display clearBtn while typing
 wikiSearch.addEventListener("keyup", () => {
   clearBtn.style.visibility = "visible";
+  wikiForm.classList.add("searching");
   if (!wikiSearch.value) {
     clearBtn.style.visibility = "hidden";
+    wikiForm.classList.remove("searching");
   }
 });
