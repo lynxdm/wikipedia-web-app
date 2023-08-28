2c9db63ea655a5555ca15702b247f8e36ab9ab75
lynxdm <lynxdev32@gmail.com>
2023-08-28 15:42:23 +0000

    edit searching classlist & button hover

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const wikiSearch = document.querySelector(".wiki-search");
 const clearBtn = document.querySelector(".clear-btn");
 
 // ADD FOCUS ON CLICK
-wikiForm.addEventListener("click", () => {
-  wikiSearch.focus();
-  wikiForm.classList.add("searching");
+document.addEventListener("mousedown", (e) => {
+  if (wikiForm.contains(e.target)) {
+    wikiSearch.focus();
+    wikiForm.classList.add("searching");
+  } else {
+    wikiForm.classList.remove("searching");
+  }
 });
 
 // display clearBtn while typing
