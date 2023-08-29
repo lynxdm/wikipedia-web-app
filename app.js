e26034b4a63b89b116ef615c2d671526c5a2162a
lynxdm <lynxdev32@gmail.com>
2023-08-29 16:37:30 +0000

    add theme toggle implementation

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const wikiForm = document.querySelector(".wiki-form");
 const wikiSearch = document.querySelector(".wiki-search");
 const clearBtn = document.querySelector(".clear-btn");
+const themeToggle = document.querySelector(".theme-toggle label");
+
+//******THEME TOGGLE IMPLEMENTATION******
+// check if dark theme was applied in previous usage
+let theme = localStorage.getItem("theme");
+if (theme) {
+  document.body.classList.add("dark-theme");
+  document.querySelector(".theme-toggle input").checked = true;
+}
+// event listener for toggle button
+themeToggle.addEventListener("click", () => {
+  // add theme class to body
+  document.body.classList.toggle("dark-theme");
+  //  setting local storage
+  localStorage.setItem("theme", "dark-theme");
+  if (!document.body.classList.contains("dark-theme")) {
+    localStorage.removeItem("theme");
+  }
+});
 
 // ADD FOCUS ON CLICK
 document.addEventListener("mousedown", (e) => {
